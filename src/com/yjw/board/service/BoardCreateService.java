df4947bbb1f8ca265e5bbc33cc08cba80fe2e74d
Riudiu <lacvert13@gmail.com>
2021-08-17 18:46:41 +0900

    use login session

--- src/com/yjw/board/service/BoardCreateService.java
+++ src/com/yjw/board/service/BoardCreateService.java
@@ -13,18 +13,17 @@ public class BoardCreateService implements InterBoardService {
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		
 		// 세션 처리
-//		HttpSession session = request.getSession();
-//		String idSession = (String)session.getAttribute("session_id");
-//
-//		if(idSession == null) {
-//			try {
-//				String ui = "login.jsp";
-//				RequestDispatcher rd = request.getRequestDispatcher(ui);
-//				rd.forward(request, response);
-//			} catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
+		HttpSession session = request.getSession();
+		String idSession = (String)session.getAttribute("session_id");
+
+		if(idSession == null) {
+			try {
+				RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+				rd.forward(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 		try {
 			request.setCharacterEncoding("utf-8");
@@ -32,10 +31,12 @@ public class BoardCreateService implements InterBoardService {
 			
 			String b_title = request.getParameter("title");
 			String b_content = request.getParameter("content");
+			String m_id = request.getParameter("id");
 			
 			BoardVO board = new BoardVO();
 			board.setB_title(b_title);
 			board.setB_content(b_content);
+			board.setM_id(m_id);
 			
 			BoardDAO dao = BoardDAO.getInstance();
 			int resultCode = dao.writeBoard(board);
