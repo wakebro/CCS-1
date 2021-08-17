abbed8c3de291332888bbe86c3227e69aa15349e
Riudiu <lacvert13@gmail.com>
2021-08-17 18:50:13 +0900

    add getBoardList

--- src/com/yjw/board/model/BoardDAO.java
+++ src/com/yjw/board/model/BoardDAO.java
@@ -2,6 +2,10 @@ package com.yjw.board.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -38,7 +42,7 @@ public class BoardDAO {
 		PreparedStatement pstmt = null;
 		
 		int resultCode;
-		String sql = "INSERT INTO board (b_title, b_content, b_date, b_view) VALUES (?, ?, now(), 0)";
+		String sql = "INSERT INTO board (b_title, b_content, m_id, b_date, b_view) VALUES (?, ?, ?, now(), 0)";
 		
 		try {
 			con = ds.getConnection();
@@ -46,6 +50,7 @@ public class BoardDAO {
 			
 			pstmt.setString(1, board.getB_title());
 			pstmt.setString(2, board.getB_content());
+			pstmt.setString(3, board.getM_id());
 			pstmt.executeUpdate();
 			
 			// 입력 확인
@@ -70,6 +75,52 @@ public class BoardDAO {
 	} 
 	// END writeBoard
 	
+	
+	public List<BoardVO> getBoardList() {
+		
+		List<BoardVO> boardList = new ArrayList<>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT * FROM board ORDER BY b_no DESC";
+		
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				BoardVO board = new BoardVO();
+				board.setB_no(rs.getInt("b_no"));
+				board.setM_id(rs.getString("m_id"));
+				board.setB_title(rs.getString("b_title"));
+				board.setB_content(rs.getString("b_content"));
+				board.setB_date(rs.getTimestamp("b_date"));
+				board.setB_view(rs.getInt("b_view"));
+				
+				boardList.add(board);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(con!=null && !con.isClosed()) {
+					con.close();
+				}
+				if(pstmt!=null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+				if(rs!=null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return boardList;
+	}  
+	// END getBoardList
 	
 	
 	
