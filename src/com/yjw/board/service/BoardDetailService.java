5f05f19aeb49e59a150e046dc7f562c4227cb679
Riudiu <lacvert13@gmail.com>
2021-08-18 18:32:27 +0900

    Simplify code

--- src/com/yjw/board/service/BoardDetailService.java
+++ src/com/yjw/board/service/BoardDetailService.java
@@ -1,5 +1,38 @@
 package com.yjw.board.service;
 
-public class BoardDetailService {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.yjw.board.model.BoardDAO;
+import com.yjw.board.model.BoardVO;
+
+public class BoardDetailService implements InterBoardService{
+	public void execute (HttpServletRequest request, HttpServletResponse response) {
+		// 세션 검사
+		HttpSession session = request.getSession();
+		String idSession = (String)session.getAttribute("session_id");
+
+		if(idSession == null) {
+			try {
+				RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+				rd.forward(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		String b_no = request.getParameter("b_no");
+		
+		BoardDAO dao = BoardDAO.getInstance();
+		
+		// 조회수 1씩 증가
+		dao.upView(b_no);
+		
+		BoardVO board = dao.getBoardDetail(b_no);
+		System.out.println(board); // 디버깅
+		
+		request.setAttribute("board", board);
+	}
 }
