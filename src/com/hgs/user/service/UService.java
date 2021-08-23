8f5b29bb37580dd2f9a2db882a65a36dcec46a83
Riudiu <lacvert13@gmail.com>
2021-08-23 15:00:54 +0900

    병합하기 전 세팅

--- src/com/hgs/user/service/UService.java
+++
@@ -1,8 +0,0 @@
-package com.hgs.user.service;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface UService {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
